519672f0519a51355872a1d60a077b413595028c
texnoman <texnoman1@yandex.ru>
2023-12-11 20:56:29 +0300

    HW1

---
+++ Семинар1/helloworld.c
@@ -0,0 +1,39 @@
+
+
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+    printf("task 1. \n");
+    printf(" \n");
+	printf("Hello World \n");
+	printf(" \n");
+	printf("task 2. \n");
+	printf(" \n");
+	printf("Let's \n");
+	printf("  go \n");
+	printf("   to walk \n");
+	printf(" \n");
+	printf("task 3. \n");
+	printf(" \n");
+	printf("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HH HH\n");
+	printf(" ZZZZZ\n");
+	printf(" \n");
+	printf("task 4. \n");
+	printf(" __    __ \n");
+	printf("/  \\__/  \\ \n");
+	printf("\\__/  \\__/ \n");
+	printf("/  \\__/  \\ \n");
+	printf("\\__/  \\__/\n");
+	
+	printf(" \n");
+     
+     
+	return 0;
+}
+
